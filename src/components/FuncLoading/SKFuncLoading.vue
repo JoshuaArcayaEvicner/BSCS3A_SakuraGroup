e5b8c59cc1625f77fdf4f47c7c41431d3c18c71d
Ralph Daniel Delvo <131005897+RalphDanielDelvo@users.noreply.github.com>
2023-06-12 20:02:36 +0800

    Added SKFunction Loading Component

---
+++ src/components/FuncLoading/SKFuncLoading.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="loading-container" v-if="loading">
+      <div class="loading-spinner"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SKLoading',
+    props: {
+      loading: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .loading-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+  
+  .loading-spinner {
+    border: 4px solid pink;
+    border-left-color: #FF0090;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    animation: spin 1s linear infinite;
+  }
+  
+  @keyframes spin {
+    to { transform: rotate(360deg); }
+  }
+  </style>
+  
