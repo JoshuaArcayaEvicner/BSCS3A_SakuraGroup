36b638573389b8273a6fd629a9726e24408ac7c5
angelinepenaflor <penafloraj@gmail.com>
2023-06-12 20:10:25 +0800

    Added Switch Component

---
+++ src/components/Switch/SKSwitch.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="switch" :class="{ checked: isChecked }" @click="toggleSwitch">
+      <div class="switch-handle"></div>
+    </div>
+  </template>
+  
+  <script>
+
+  export default {
+    data() {
+      return {
+        isChecked: false,
+      };
+    },
+    methods: {
+      toggleSwitch() {
+        this.isChecked = !this.isChecked;
+        this.$emit('change', this.isChecked);
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .switch {
+    position: relative;
+    width: 60px;
+    height: 30px;
+    border-radius: 15px;
+    background-color: #ccc;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+  
+  .switch-handle {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: 26px;
+    height: 26px;
+    border-radius: 13px;
+    background-color: #fff;
+    transition: transform 0.3s;
+  }
+  
+  .switch.checked {
+    background-color: #ec2ec3;
+  }
+  
+  .switch.checked .switch-handle {
+    transform: translateX(30px);
+  }
+  </style>
+  
