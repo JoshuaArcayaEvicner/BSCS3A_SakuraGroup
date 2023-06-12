659cd1c6932844efa349504abc2ef53edd1b17b4
angelinepenaflor <penafloraj@gmail.com>
2023-06-12 20:09:46 +0800

    added Progress Component

---
+++ src/components/Progress/SKProgress.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="progress-bar">
+      <div class="progress" :style="{ width: progress + '%' }"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      progress: {
+        type: Number,
+        required: true,
+        validator: (value) => value >= 0 && value <= 100,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .progress-bar {
+    width: 100%;
+    height: 20px;
+    background-color: #ebbce2;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  
+  .progress {
+    height: 100%;
+    background-color: #FF0090;
+    transition: width 0.3s ease-in-out;
+  }
+  </style>
+  
