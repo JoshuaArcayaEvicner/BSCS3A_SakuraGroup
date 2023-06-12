dc51cc0ea3c31162dbed8ab7c00d8c7df9264211
Gladys Ann Gonzales <130980881+GladysAnnGonzales@users.noreply.github.com>
2023-06-12 19:55:25 +0800

    Added SKCheckbox Component

---
+++ src/Checkbox/SKCheckbox.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          :checked="isChecked"
+          :disabled="isDisabled"
+          @change="handleChange"
+        />
+        {{ label }}
+      </label>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      value: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      label: {
+        type: String,
+        default: ""
+      }
+    },
+    computed: {
+      isChecked: {
+        get() {
+          return this.value;
+        },
+        set(newValue) {
+          this.$emit("input", newValue);
+        }
+      },
+      isDisabled() {
+        return this.disabled;
+      }
+    },
+    methods: {
+      handleChange(event) {
+        if (!this.isDisabled) {
+          this.isChecked = event.target.checked;
+        }
+      }
+    }
+  };
+  </script>
+  
