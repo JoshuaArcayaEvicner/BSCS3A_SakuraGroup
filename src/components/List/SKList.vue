421c792bf88141280cbc0a93df405d56c377123c
Joshua Evicner <119050539+JoshuaArcayaEvicner@users.noreply.github.com>
2023-06-12 20:05:27 +0800

    Added List Component

---
+++ src/components/List/SKList.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="list">
+      <li v-for="item in items" :key="item.id">{{ item.label }}</li>
+    </ul>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SKList',
+    props: {
+      items: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .list {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  .list li {
+    margin-bottom: 5px;
+    color: #E11299;
+  }
+  </style>
